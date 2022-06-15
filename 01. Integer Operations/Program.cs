4437ea2e47e7a79b6783ecc5923626e174440c1a
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-15 16:50:41 +0300

    Add files via upload

---
+++ 01. Integer Operations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACITY = 255;
+            int nLines = int.Parse(Console.ReadLine());
+            int totalQuantity = CAPACITY;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int currQuantity = int.Parse(Console.ReadLine());
+                if (totalQuantity - currQuantity>=0)
+                {
+                    totalQuantity -= currQuantity;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+
+                }
+            }
+            int totalFieldQuantity = CAPACITY - totalQuantity;
+            Console.WriteLine(totalFieldQuantity);
+
+          
+
+            
+        }
+    }
+}
